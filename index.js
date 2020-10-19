1991f49c89473d8044271faed45f8a8a77dd1489
Ana Luiza Caldas <analuizacaldas7@gmail.com>
2020-10-19 19:02:13 -0300

    moeda de saída

--- index.js
+++ index.js
@@ -1,22 +1,68 @@
-let moeda = 0;
+let moedaEntrada = {};
 let ValorAConverter = 0;
 let conversao = 0;
+let moedaSaida = {};
 
+// adicionar mais tipos de moeda - colocar no select tambem
+const TipodeMoedas = {
 
-function onChangeSelecionarMoeda(event){
+    Dolar: {
+        valor: 5.30,
+        type: 'USD'
+    },
+
+    Real: {
+        valor: 1.0,
+        type: 'BRL'
+    },
+    
+    Euro: {
+        valor: 6.30,
+        type: 'EUR'
+    },
+
+    'Peso Argentino': {
+        valor: 0.073,
+        type: 'ARS'
+    },
+
+}
+
+function onChangeSelecionarMoedaEntrada(event){
+    let moeda = event.target.value; 
     console.log(event.target.value);
-    moeda = Number(event.target.value);
+    moedaEntrada = TipodeMoedas[moeda];
+}
+function onChangeSelecionarMoedaSaida(event){
+    let moeda = event.target.value; 
+    console.log(event.target.value);
+    moedaSaida = TipodeMoedas[moeda];
 }
 
 function onChangeInputValue(event){ 
     ValorAConverter = Number(event.target.value);
     console.log(event.target.value);
 }
+// Moeda base = real 
+
+//.valor = atributo da valor da moeda e .type = tipo 'usd , 'brl'
 
 function onClickConverter(){
-    conversao = (moeda * ValorAConverter);
-    let conversaoDisplay = conversao.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    if (moedaEntrada.valor == 1){
+        conversao = (ValorAConverter / moedaSaida.valor);
+        
+    } 
+    else if (moedaSaida.valor == 1){
+
+        conversao = (ValorAConverter * moedaEntrada.valor);
+    }
+    else {
+        conversao = (moedaEntrada.valor * ValorAConverter / moedaSaida.valor);
+    }
+   
+  
+    let conversaoDisplay = conversao.toLocaleString('pt-BR', { style: 'currency', currency: moedaSaida.type });
     console.log(conversao);
     document.getElementById("ValorConvertido").innerHTML = conversaoDisplay;
 }
-     
+
