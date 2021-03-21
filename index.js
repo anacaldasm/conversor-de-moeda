89d44ef2f8fb2ee3f418e836e30cec1174753e96
Ana Luiza Caldas <67129615+anacaldasm@users.noreply.github.com>
2021-03-21 01:14:31 -0300

    ajusta responsividade

--- index.js
+++ index.js
@@ -1,61 +1,55 @@
-
 const TipodeMoedas = {
+  Dolar: {
+    valor: 5.3,
+    type: "USD",
+  },
 
-    Dolar: {
-        valor: 5.30,
-        type: 'USD'
-    },
+  Real: {
+    valor: 1.0,
+    type: "BRL",
+  },
 
-    Real: {
-        valor: 1.0,
-        type: 'BRL'
-    },
+  Euro: {
+    valor: 6.3,
+    type: "EUR",
+  },
 
-    Euro: {
-        valor: 6.30,
-        type: 'EUR'
-    },
-
-    'Peso Argentino': {
-        valor: 0.073,
-        type: 'ARS'
-    },
-
-}
+  "Peso Argentino": {
+    valor: 0.073,
+    type: "ARS",
+  },
+};
 //adicionando as options ao select no html
 function addCurrenciesToSelect(elementId) {
-    const listaDeMoeads = Object.keys(TipodeMoedas); // pega lista de moedas - chave do map
-    document.getElementById(elementId).innerHTML += listaDeMoeads.map(
-        (moeda) => {
-            return `<option value="${moeda}">${moeda}</option>`;
-        }
-    );
+  const listaDeMoeads = Object.keys(TipodeMoedas); // pega lista de moedas - chave do map
+  document.getElementById(elementId).innerHTML += listaDeMoeads.map((moeda) => {
+    return `<option value="${moeda}">${moeda}</option>`;
+  });
 }
 
 addCurrenciesToSelect("select-entrada");
 addCurrenciesToSelect("select-saida");
 
-// Moeda base = real 
+// Moeda base = real
 
 function onClickConverter() {
-    // Get Input value
-    const moedaEntrada =
-        TipodeMoedas[document.getElementById("select-entrada").value];
-    const moedaSaida =
-        TipodeMoedas[document.getElementById("select-saida").value];
-    const valorAConverter = Number(document.getElementById("input-value").value);
+  // Get Input value
+  const moedaEntrada =
+    TipodeMoedas[document.getElementById("select-entrada").value];
+  const moedaSaida =
+    TipodeMoedas[document.getElementById("select-saida").value];
+  const valorAConverter = Number(document.getElementById("input-value").value);
 
-    //conversao
-    const convertionRatio = moedaEntrada.valor / moedaSaida.valor;
-    const convertedValue = valorAConverter * convertionRatio;
+  //conversao
+  const convertionRatio = moedaEntrada.valor / moedaSaida.valor;
+  const convertedValue = valorAConverter * convertionRatio;
 
-    //.valor = atributo da valor da moeda e .type = tipo 'usd , 'brl'
+  //.valor = atributo da valor da moeda e .type = tipo 'usd , 'brl'
 
-    let conversaoDisplay = convertedValue.toLocaleString('pt-BR', {
-        style: 'currency',
-        currency: moedaSaida.type
-    });
-    console.log(convertedValue);
-    document.getElementById("converted-value").innerHTML = conversaoDisplay;
+  let conversaoDisplay = convertedValue.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: moedaSaida.type,
+  });
+  console.log(convertedValue);
+  document.getElementById("converted-value").innerHTML = conversaoDisplay;
 }
-
